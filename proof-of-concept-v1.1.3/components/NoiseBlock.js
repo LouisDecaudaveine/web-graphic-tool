d744da51ea4094c65e884eac22b2cbfcbf85137b
LouisDecaudaveine <tz20989@bristol.ac.uk>
2023-02-13 18:39:31 +0000

    working static noise node, next step is to make a connection node

--- proof-of-concept-v1.1.3/components/NoiseBlock.js
+++ proof-of-concept-v1.1.3/components/NoiseBlock.js
@@ -1,13 +1,75 @@
-function NoiseBlock(font, dimensions = 1 ,seed = -1) {
+function NoiseBlock(font, resolution = [200,100], dimensions = 1 ,seed = -1) {
 
     this.seed = seed > 0 ? seed : (int)(Math.random(0,100000));
-    this.base = new PCB(100,400, 100,100, "NOISE",font);
-    this.dimensions = dimensions > 5 ? 5 : dimensions;
-    this.isPaused = false;
-    this.position = Math.zeros(dimensions);
-    this.defaultSpread = 0.01
+    this.base = new PCB(100,100,300,200, "NOISE",font);
 
+    this.resolution = [...resolution];
+    //The amount of dimensions mean:
+    //if 1 you get a point along 1 dimension
+    //if 2 you get a still 2D image of noise
+    //if 3D you get a moivng 2D image of noise
+    this.dimensions = dimensions > 3  || dimensions < 1 ? 3 : dimensions;
+    this.isPaused = false;
+    this.position = (dimensions === 1) ? [0] : [0,0,0];
+    this.defaultSpread = 0.1
+    this.speed = 0.01;
+
+    noiseSeed(this.seed);
+
+    this.getNoise = () => {
+        if(dimensions === 1)
+            return [noise(this.position[0])];
+        if(dimensions === 2 || dimensions === 3 ){
+            let slice = [];
+            for(let i=0; i<resolution[1];i++){
+                slice.push([]);
+                let yNoise = this.position[1] + this.defaultSpread*i;
+                for(let j=0; j<resolution[0];j++){
+                    let xNoise = this.position[0] + this.defaultSpread*j;
+                    slice[i][j] = noise(xNoise, yNoise, this.position[2]);
+                }
+            }
+            return slice;
+        }
+    }
+    this.value = this.getNoise();
+
+    this.updateX = (speed) => {
+        this.position[0] += speed;
+    }
+    this.updateY = (speed) => {
+        this.position[1] += speed;
+    }
+    this.updateZ = (speed) => {
+        this.position[2] += speed;
+    }
+
+    this.threeDMove = () => {
+        this.updateZ(this.speed);
+        this.value = this.getNoise();
+    }
+
+    //this is if you just want to visualise noise over your canvas
+    //should not be used often, extremely innificient
+    //noise should be used as data, sampled at relatively low rates. aka: low resolution
+    this.output = () => {
+        let xResConv = this.resolution[0]/width;
+        let yResConv = this.resolution[1]/height;
+        loadPixels();
+            for(let yY = 0; yY < height; yY++){
+                for(let xX = 0; xX < width; xX++){
+                    set(xX,yY, color(
+                        this.value[(int)(yY*yResConv)][(int)(xX*xResConv)]*255));
+                }  
+            }
+        updatePixels();
+    }
 
     
+
+    //NEED TO FIGURE OUT INHERITANCE
+    //TEMP SOLUTION AINT THE ONE: 
+
+
 
 }
--- proof-of-concept-v1.1.3/sketch.js
+++ proof-of-concept-v1.1.3/sketch.js
@@ -12,30 +12,36 @@ function setup() {
   inputMP3 = createFileInput(() => {
     col = 125
   });
-  temp.push(new PCB(100,100,300,200, "Giving", font));
-    temp[0].addSlider("test", 0, 10, temp[0]);
-    temp[0].addSlider("test2", 0, 10, temp[0]);
-    temp[0].addSlider("test2", 0, 10, temp[0]);
-  temp.push(new PCB(100,400,300,200, "Receiving", font));
-    temp[1].addSlider("test", 0, 10, temp[0]);
-    temp[1].addSlider("test2", 0, 10, temp[0]);
+  // temp.push(new PCB(100,100,300,200, "Giving", font));
+  //   temp[0].addSlider("test", 0, 10, temp[0]);
+  //   temp[0].addSlider("test2", 0, 10, temp[0]);
+  //   temp[0].addSlider("test2", 0, 10, temp[0]);
+  // temp.push(new PCB(100,400,300,200, "Receiving", font));
+  //   temp[1].addSlider("test", 0, 10, temp[0]);
+  //   temp[1].addSlider("test2", 0, 10, temp[0]);
+  temp.push(new NoiseBlock(font,[200,100],3))
 }
 
 
 function draw() {
   background(col);
-  temp.forEach((block) => block.show())
+  // temp[0].threeDMove();
+  // temp[0].output();
+  temp.forEach((block) => block.base.show())
+  
+  
   // noLoop()
+  // console.log((int)(frameRate()))
   
 }
 function mousePressed() {
   temp.forEach((block) => {
-    if(mouseX > block.x &&
-      mouseX < block.x +block.w &&
-      mouseY > block.y &&
-      mouseY < block.y+1.5*block.titleSize){
-      block.anchorPoint = [Math.floor(mouseX),Math.floor(mouseY)]
-      block.anchored = true;
+    if(mouseX > block.base.x &&
+      mouseX < block.base.x +block.base.w &&
+      mouseY > block.base.y &&
+      mouseY < block.base.y+1.5*block.base.titleSize){
+      block.base.anchorPoint = [Math.floor(mouseX),Math.floor(mouseY)]
+      block.base.anchored = true;
     }
   });
 }
@@ -44,8 +50,8 @@ function mouseDragged() {
     //this is for moving the blocks
     //if an anchor point has been set then move block to mouse
     //and update the anchor point
-    if(block.anchored) {
-      block.move();
+    if(block.base.anchored) {
+      block.base.move();
     }
   })
 }
@@ -54,8 +60,8 @@ function mouseReleased(){
 
   temp.forEach((block) => {
     //this is resetting the anchor points of each block
-    block.anchorPoint = [-1,-1];
-    block.anchored = false;
+    block.base.anchorPoint = [-1,-1];
+    block.base.anchored = false;
   })
 }
 
