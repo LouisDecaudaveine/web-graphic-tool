77eda789380f4242fe1baa794448e5ba28c7dc5a
LouisDecaudaveine <tz20989@bristol.ac.uk>
2023-03-08 18:41:20 +0000

    implemented fonts and started on sketch editing

--- MVP/mvp/src/P5Wrapper.js
+++ MVP/mvp/src/P5Wrapper.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sketch from "react-p5";
 import  {ReadParser}  from "./EditorParser"
+import mirrorFont from "./assets/mirror82_v2.otf"
 
 export default (props) => {
-
+	//objects: these are all the nodes that you can see on the vpl 
+	//layers: is a list of all the visual component they are rendered from lowest index to highest index
 	const [objects,layers] = ReadParser(props.VPLState);
+
+	let needsResizing = false
+
+	//this queues a resize of canvas whenever the sketch VPL component changes its dimensions
+	useEffect((p5) => {
+		needsResizing = true;
+	}, [props.width, props.height])
+
+	let font;
+	const preload = (p5) => {
+		font = p5.loadFont(mirrorFont);
+	}
 
 	const setup = (p5, canvasParentRef) => {
 		// use parent to render the canvas in this ref
 		// (without that p5 will render the canvas outside of your component)
 		p5.createCanvas(props.width, props.height).parent(canvasParentRef);
+		p5.textFont(font);
 	};
 
 	const draw = (p5) => {
+		if(needsResizing){
+			p5.resizeCanvas(props.width,props.height);
+			needsResizing = false;
+		}
+
 		p5.background(0);
 		// p5.ellipse(props.width/2, props.height/2, 70, 70);
 
@@ -23,10 +43,26 @@ export default (props) => {
 		layers.forEach(layer => {
 			layer.show(p5);
 		})
-		
 	};
 
-	return <Sketch setup={setup} draw={draw}/>;
+	const mousePressed = (p5) => {
+
+	} 
+
+	const mouseDragged = (p5) => {
+
+	}
+
+	const mouseReleased = (p5) => {
+
+	}
+
+	return <Sketch setup={setup} 
+		draw={draw} 
+		preload={preload} 
+		mousePressed={mousePressed}
+		mouseDragged={mouseDragged} 
+		mouseReleased={mouseReleased} />;
 };
 
 
--- MVP/mvp/src/p5Components/TextComp.js
+++ MVP/mvp/src/p5Components/TextComp.js
@@ -2,14 +2,22 @@ export default function TextComp(VPLNode){
 
     console.log("within text comp: ",VPLNode);
 
+    //these is all the properties relating to the VPL
     this.id = VPLNode.id;
     this.posX = VPLNode.data.xPos;
     this.posY = VPLNode.data.yPos;
     this.content = VPLNode.data.text;
     this.size = VPLNode.data.size;
 
+    //these one here are for anything to do with the sketch
+    this.anchored = false;
+    this.boundingBox = {
+        width: this.size,
+        height: this.size,
+    };
+
     this.show = (p5) => {
-        p5.push();
+        p5.push(); 
             p5.fill(255);
             p5.textSize(this.size);
             p5.text(this.content, this.posX, this.posY);
